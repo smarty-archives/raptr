56c47aea58bd00cd61be7a6f043c1632b52a71ea
Oliver, Jonathan <git@jonathanoliver.com>
2014-09-24 22:42:16 -0600

    Returning a meta record on each next read.

--- eventstore.go
+++ eventstore.go
@@ -8,40 +8,38 @@ import (
 )
 
 type EventStore struct {
+	sequence uint64
 	handle   *os.File
 	reader   *bufio.Reader
-	decoder  *json.Decoder
-	sequence uint64
 }
 
 func NewEventStore(filename string) (*EventStore, error) {
 	if handle, err := os.Open(filename); err != nil {
 		return nil, err
 	} else {
-		return NewEventStoreReader(handle), nil
-	}
-}
-func NewEventStoreReader(handle *os.File) *EventStore {
-	reader := bufio.NewReaderSize(handle, bufferSize)
-	return &EventStore{
-		handle:  handle,
-		reader:  reader,
-		decoder: json.NewDecoder(reader),
+		return &EventStore{
+			handle: handle,
+			reader: bufio.NewReaderSize(handle, bufferSize),
+		}
 	}
 }
 
-func (this *EventStore) Next() (interface{}, error) {
+func (this *EventStore) Next() (*StorageMeta, error) {
 	if metaText, err := this.reader.ReadString('\n'); err != nil {
 		return nil, err
 	} else if meta, err := ParseMeta(metaText); err != nil {
 		return nil, err
-	} else if err := this.decoder.Decode(meta.Message); err != nil {
+	} else if payload, err := this.reader.ReadBytes('\n'); err != nil {
+		return nil, err
+	} else if len(payload) == 0 {
+		return nil, err
+	} else if err := json.Unmarshal(payload, meta.Message); err != nil {
 		return nil, err
 	} else if meta.Sequence != this.sequence+1 {
 		return nil, errors.New("Record out of sequence")
 	} else {
 		this.sequence++
-		return meta.Message, nil
+		return meta, nil
 	}
 }
 
