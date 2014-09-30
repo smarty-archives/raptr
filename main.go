450d5a83fb1f0594bdcbfa08b53914ef04b3f149
Oliver, Jonathan <git@jonathanoliver.com>
2014-09-30 05:40:18 -0600

    Removing eventstore behavior.

--- main.go
+++ main.go
@@ -1,35 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
-
 func main() {
-	handle, err := os.Open("test.json")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer handle.Close()
-
-	reader := NewEventStoreReader(handle)
-	writer := NewEventStoreWriter(0, os.Stdout)
-
-	for {
-		if record, err := reader.Read(); err != nil {
-			fmt.Println("Reading:", err)
-			break
-		} else if err := writer.Write(record); err != nil {
-			fmt.Println("Writing:", err)
-			break
-		}
-	}
 }
-
-// various projections for different purposes:
-// 1. validation of incoming input
-// 2. generation of apt index files
-
-// all events provided to all projections (simple)
-// and they decide what to do
