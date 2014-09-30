d86f9262c7bdc0be695c41510bdc7224793148e2
Jonathan Duncan <jonathan@nacnud.com>
2014-09-30 19:35:04 +0000

    Implemented parallel remote to handle multiple requests at the same time.

--- parallel_remote.go
+++ parallel_remote.go
@@ -1,5 +1,7 @@
 package main
 
+import "sync"
+
 // Takes a set of operations and performs them in parallel such that each
 // operation happens side by side. The result slice from each operation will
 // be returned in the same order, e.g. the first request correlates to the
@@ -7,25 +9,85 @@ package main
 // each set parallel operations will block until all desired operations have
 // completed.
 type ParallelRemote struct {
-	remotes []Remote
+	inner Remote
 }
 
-func NewParallelRemote(remotes []Remote) *ParallelRemote {
-	return &ParallelRemote{remotes: remotes}
+func NewParallelRemote(inner Remote) *ParallelRemote {
+	return &ParallelRemote{inner: inner}
 }
 
 func (this *ParallelRemote) Put(requests ...PutRequest) []PutResponse {
-	return []PutResponse{}
+	var waiter sync.WaitGroup
+	waiter.Add(len(requests))
+	responses := make([]PutResponse, len(requests))
+
+	for i, request := range requests {
+		go func() {
+			responses[i] = this.inner.Put(request)
+			waiter.Done()
+		}()
+	}
+
+	waiter.Wait()
+	return responses
 }
 func (this *ParallelRemote) Get(requests ...GetRequest) []GetResponse {
-	return []GetResponse{}
+	var waiter sync.WaitGroup
+	waiter.Add(len(requests))
+	responses := make([]GetResponse, len(requests))
+
+	for i, request := range requests {
+		go func() {
+			responses[i] = this.inner.Get(request)
+			waiter.Done()
+		}()
+	}
+
+	waiter.Wait()
+	return responses
 }
 func (this *ParallelRemote) Delete(requests ...DeleteRequest) []DeleteResponse {
-	return []DeleteResponse{}
+	var waiter sync.WaitGroup
+	waiter.Add(len(requests))
+	responses := make([]DeleteResponse, len(requests))
+
+	for i, request := range requests {
+		go func() {
+			responses[i] = this.inner.Delete(request)
+			waiter.Done()
+		}()
+	}
+
+	waiter.Wait()
+	return responses
 }
 func (this *ParallelRemote) Head(requests ...HeadRequest) []HeadResponse {
-	return []HeadResponse{}
+	var waiter sync.WaitGroup
+	waiter.Add(len(requests))
+	responses := make([]HeadResponse, len(requests))
+
+	for i, request := range requests {
+		go func() {
+			responses[i] = this.inner.Head(request)
+			waiter.Done()
+		}()
+	}
+
+	waiter.Wait()
+	return responses
 }
 func (this *ParallelRemote) List(requests ...ListRequest) []ListResponse {
-	return []ListResponse{}
+	var waiter sync.WaitGroup
+	waiter.Add(len(requests))
+	responses := make([]ListResponse, len(requests))
+
+	for i, request := range requests {
+		go func() {
+			responses[i] = this.inner.List(request)
+			waiter.Done()
+		}()
+	}
+
+	waiter.Wait()
+	return responses
 }
