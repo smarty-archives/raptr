b86fbbef6227366b7205c0fe4d98f12add1fede9
Oliver, Jonathan <git@jonathanoliver.com>
2014-10-08 13:12:16 -0600

    Implementing upload task behavior.

--- manifest/manifest_file.go
+++ manifest/manifest_file.go
@@ -1,3 +1,25 @@
 package manifest
 
+import (
+	"io"
+	"path"
+)
+
 type ManifestFile struct{}
+
+func NewManifestFile() *ManifestFile {
+	return &ManifestFile{}
+}
+func ParseManifest(reader io.Reader) (*ManifestFile, error) {
+	return nil, nil
+}
+func BuildPath(category, bundle, version string) string {
+	return path.Join("/", category, bundle, version, "manifest") // TODO: gz?
+}
+
+func (this *ManifestFile) Add(filename string) bool {
+	return false // TODO
+}
+func (this *ManifestFile) Bytes() []byte {
+	return []byte{} // TODO
+}
--- tasks/upload_task.go
+++ tasks/upload_task.go
@@ -1,27 +1,68 @@
 package tasks
 
 import (
-	"github.com/smartystreets/raptr/messages"
+	"log"
+	"os"
+
+	"github.com/smartystreets/raptr/manifest"
 	"github.com/smartystreets/raptr/storage"
 )
 
 type UploadTask struct {
 	local  storage.Storage
 	remote storage.Storage
+	multi  *storage.MultiStorage
 }
 
 func NewUploadTask(local, remote storage.Storage) *UploadTask {
-	return &UploadTask{local: local, remote: remote}
+	return &UploadTask{local: local, remote: remote, multi: storage.NewMultiStorage(remote)}
 }
 
-func (this *UploadTask) Upload(command messages.UploadCommand) error {
-	// discover info about the files, e.g. deb/dsc, version info, etc.
-	// check the integrity of any files listed in the dsc
-	// should this be done during the validation phase of the command
-	// such that it's not duplicated here? (e.g. during concurrency retry
-	// attempts we shouldn't perform the above steps again).
+// the list of files to upload along with integrity checks, etc.
+// will be done during the validation phase (prior to this)
+// such that any concurrency-related errors that might occur here
+// won't cause the validation to be re-run
+func (this *UploadTask) Upload(category, bundle, version string, files []string) error {
+	manifestPath := manifest.BuildPath(category, bundle, version)
+	manifestResponse := this.remote.Get(storage.GetRequest{Path: manifestPath})
+	if manifestFile, err := parseManifestResponse(manifestResponse); err != nil {
+		return err // unable to access remote manifest
+	} else if err := this.uploadBundleFiles(files, manifestFile); err != nil {
+		return err // one or more file uploads failed
+	} else {
+		contents := storage.NewReader(manifestFile.Bytes())
+		request := storage.PutRequest{Path: manifestPath, Contents: contents}
+		return this.remote.Put(request).Error
+	}
+}
 
-	// download remote manifest (if any) to see packages are already there (based upon name) and skip them
-	// upload the files
-	// upload updated manifest (with concurrency checks)
+func parseManifestResponse(response storage.GetResponse) (*manifest.ManifestFile, error) {
+	if response.Error != nil && os.IsNotExist(response.Error) {
+		return manifest.NewManifestFile(), nil
+	} else if response.Error != nil {
+		return nil, response.Error
+	} else if parsed, err := manifest.ParseManifest(response.Contents); err != nil {
+		return nil, err
+	} else {
+		return parsed, nil
+	}
+}
+func (this *UploadTask) uploadBundleFiles(files []string, manifestFile *manifest.ManifestFile) error {
+	uploads := []storage.PutRequest{}
+
+	for _, file := range files {
+		if !manifestFile.Add(file) {
+			log.Println()
+		} else {
+			uploads = append(uploads, storage.PutRequest{}) // TODO
+		}
+	}
+
+	for _, response := range this.multi.Put(uploads...) {
+		if response.Error != nil {
+			return response.Error
+		}
+	}
+
+	return nil
 }
