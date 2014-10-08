7223072e77600fe1cd6e8a6bea367c076b046199
Oliver, Jonathan <git@jonathanoliver.com>
2014-10-08 10:29:31 -0600

    Implemented a simple local filesystem that computes the checksum on read.

--- storage/filesystem_storage.go
+++ storage/filesystem_storage.go
@@ -1,28 +1,98 @@
 package storage
 
-// Enables a locally configured file system directory to serve as a remote.
-// TODO: have md5_fs and simple_fs?
-// md5_fs has md5 checksum appended to each file
-type FilesystemStorage struct {
-	filenameAppendMD5 bool
-}
+import (
+	"bufio"
+	"crypto/md5"
+	"io"
+	"os"
+	"path"
+	"path/filepath"
+)
 
-func NewFilesystemStorage(fast, slow Storage) *FilesystemStorage {
+type FilesystemStorage struct{}
+
+func NewFilesystemStorage() *FilesystemStorage {
 	return &FilesystemStorage{}
 }
 
 func (this *FilesystemStorage) Put(request PutRequest) PutResponse {
-	return PutResponse{}
-}
-func (this *FilesystemStorage) Get(request GetRequest) GetResponse {
-	return GetResponse{}
-}
-func (this *FilesystemStorage) List(request ListRequest) ListResponse {
-	return ListResponse{}
+	panic("Not implemented")
 }
 func (this *FilesystemStorage) Head(request HeadRequest) HeadResponse {
-	return HeadResponse{}
+	if fullPath, err := filepath.Abs(request.Path); err != nil {
+		return HeadResponse{Path: request.Path, Error: err}
+	} else if info, err := os.Stat(fullPath); err != nil {
+		return HeadResponse{Path: request.Path, Error: err}
+	} else if md5hash, err := computeMD5(fullPath); err != nil {
+		return HeadResponse{Path: request.Path, Error: err}
+	} else {
+		return HeadResponse{
+			Path:    request.Path,
+			MD5:     md5hash,
+			Created: info.ModTime(),
+			Length:  uint64(info.Size()),
+		}
+	}
+}
+func (this *FilesystemStorage) Get(request GetRequest) GetResponse {
+	if fullPath, err := filepath.Abs(request.Path); err != nil {
+		return GetResponse{Path: request.Path, Error: err}
+	} else if info, err := os.Stat(fullPath); err != nil {
+		return GetResponse{Path: request.Path, Error: err}
+	} else if md5hash, err := computeMD5(fullPath); err != nil {
+		return GetResponse{Path: request.Path, Error: err}
+	} else if handle, err := os.Open(fullPath); err != nil {
+		return GetResponse{Path: request.Path, Error: err}
+	} else {
+		return GetResponse{
+			Path:     request.Path,
+			Contents: handle, // implements Read/Seek/Close--application must close
+			MD5:      md5hash,
+			Created:  info.ModTime(),
+			Length:   uint64(info.Size()),
+		}
+	}
+}
+func (this *FilesystemStorage) List(request ListRequest) ListResponse {
+	if fullPath, err := filepath.Abs(request.Path); err != nil {
+		return ListResponse{Path: request.Path, Error: err}
+	} else {
+		return ListResponse{Path: request.Path, Items: walk(fullPath)}
+	}
 }
 func (this *FilesystemStorage) Delete(request DeleteRequest) DeleteResponse {
-	return DeleteResponse{}
+	panic("Not implemented")
+}
+
+func computeMD5(fullPath string) ([]byte, error) {
+	handle, err := os.Open(fullPath)
+	if err != nil {
+		return nil, err
+	}
+
+	defer handle.Close()
+	reader := bufio.NewReaderSize(handle, 1024*1024*16) // 16MB buffer when reading
+	hasher := md5.New()
+
+	if _, err := io.Copy(hasher, reader); err != nil {
+		return nil, err
+	} else {
+		return hasher.Sum(nil)[:], nil
+	}
+}
+func walk(fullPath string) []ListItem {
+	items := []ListItem{}
+	filepath.Walk(fullPath, func(directory string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		} else if err == nil {
+			items = append(items, ListItem{
+				Path:    path.Join(directory, info.Name()),
+				Created: info.ModTime(),
+				Length:  uint64(info.Size()),
+			})
+		}
+		return nil
+	})
+	return items
 }
