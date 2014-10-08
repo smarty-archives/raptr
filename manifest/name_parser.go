3554569c7b9de1857903a8a6631eab6541e4a192
Oliver, Jonathan <git@jonathanoliver.com>
2014-10-08 15:11:13 -0600

    Implemented name parser to determine debian package name and version from filename.

--- manifest/name_parser.go
+++ manifest/name_parser.go
@@ -1,11 +1,48 @@
 package manifest
 
+import (
+	"path"
+	"strings"
+)
+
 type ParsedName struct {
 	Name         string
 	Version      string
 	Architecture string
+	Container    string // if it exists
 }
 
-func ParseName(name string) ParsedName {
-	return ParsedName{}
+func ParseFilename(fullPath string) *ParsedName {
+	filename := strings.ToLower(path.Base(fullPath))
+	extension := path.Ext(filename)
+	filename = strings.TrimSuffix(filename, extension)
+	if len(extension) > 0 {
+		extension = extension[1:]
+	}
+	parts := strings.Split(filename, "_")
+
+	switch extension {
+	case "deb":
+		if len(parts) != 3 {
+			return nil
+		}
+		return &ParsedName{
+			Name:         parts[0],
+			Version:      parts[1],
+			Architecture: parts[2],
+			Container:    extension,
+		}
+	case "dsc":
+		if len(parts) != 2 {
+			return nil
+		}
+		return &ParsedName{
+			Name:         parts[0],
+			Version:      parts[1],
+			Architecture: "source",
+			Container:    extension,
+		}
+	default:
+		return nil
+	}
 }
